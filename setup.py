41fea53a6c0f04c2d84b274f3f3580016aa2e48f
Jim Dixon <jddixon@gmail.com>
2018-03-20 12:06:32 -0700

    fix corrupt .dvcz/builds

--- setup.py
+++ setup.py
@@ -1,36 +1,41 @@
 #!/usr/bin/python3
 # rnglib/setup.py
 
-""" Set up distutils for rnglib. """
+""" Setuptools project configuration for rnglib. """
 
 from os.path import exists
 from setuptools import setup
 
-LONG_DESC = None
+long_desc = None
 if exists('README.md'):
     with open('README.md', 'r') as file:
-        LONG_DESC = file.read()
+        long_desc = file.read()
 
 setup(name='rnglib',
-      version='1.3.8',
+      version='1.3.9',
       author='Jim Dixon',
       author_email='jddixon@gmail.com',
-      long_description=LONG_DESC,
-      packages=['rnglib', ],
+      long_description=long_desc,
+      packages=['rnglib'],
       package_dir={'': 'src'},
       py_modules=[],
       include_package_data=False,
       zip_safe=False,
-      scripts=[],          # front end module(s)
-      description='random number geerator library',
-      url='https:/jddixon.github.io/rnglib',
+      scripts=[],
+      ext_modules=[],
+      description='random number generator library',
+      url='https://jddixon.github.io/rnglib',
       classifiers=[
           'Development Status :: 4 - Beta',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
           'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python 2',
           'Programming Language :: Python 2.7',
+          'Programming Language :: Python 3',
           'Programming Language :: Python 3.5',
           'Programming Language :: Python 3.6',
+          'Programming Language :: Python 3.7',
           'Topic :: Software Development :: Libraries :: Python Modules',
-      ])
+      ],)
